ca03cd9a753ec0df8499c3ce0c52e535211b3391
Eduardo <eduneves95@gmail.com>
2022-07-22 11:37:20 -0300

    Finished bar navigation

--- src/routes/app.routes.tsx
+++ src/routes/app.routes.tsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import { useTheme } from 'styled-components';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import { MaterialIcons } from '@expo/vector-icons'
+import { MaterialIcons } from '@expo/vector-icons';
+import { Platform } from 'react-native';
 
 const { Navigator, Screen } = createBottomTabNavigator();
 
 import { Dashboard } from '../screens/Dashboard';
 import { Register } from '../screens/Register';
-import { Platform } from 'react-native';
+import { Resume } from '../screens/Resume';
+
 
 export function AppRoutes(){
     const theme = useTheme();
@@ -53,7 +55,7 @@ export function AppRoutes(){
             />
             <Screen 
                 name="Resumo"
-                component={Register}
+                component={Resume}
                 options= {{
                     tabBarIcon: (({ size, color }) => 
                         <MaterialIcons
