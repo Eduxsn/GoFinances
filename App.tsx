4d944ef09f02d5d2de95b6bdd5e92faf76430ccd
Eduardo <eduneves95@gmail.com>
2022-07-26 16:40:48 -0300

    Refactored Login

--- App.tsx
+++ App.tsx
@@ -13,8 +13,11 @@ import {
   Poppins_700Bold,
 } from '@expo-google-fonts/poppins'
 
-import { AppRoutes } from './src/routes/app.routes';
-import { NavigationContainer } from '@react-navigation/native';
+import { Routes } from './src/routes';
+
+import {SignIn} from './src/screens/SignIn';
+
+import { AuthProvider, useAuth,  } from './src/hooks/auth';
 
 export default function App() {
   const [fontsLoaded] = useFonts({
@@ -22,19 +25,20 @@ export default function App() {
     Poppins_500Medium,
     Poppins_700Bold,
   });
+
+  const { userStorageLoading } = useAuth();
   
-  if(!fontsLoaded){
+  if(!fontsLoaded || userStorageLoading ){
     return <AppLoading />
   }
 
 
   return (
     <ThemeProvider theme={theme}>
-      <NavigationContainer>
         <StatusBar barStyle='light-content'/>
-        <AppRoutes />
-      </NavigationContainer>
+        <AuthProvider>
+          <Routes />
+        </AuthProvider>
     </ThemeProvider>
-
   );
 }
